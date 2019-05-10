813245e3403c8b8e5a4e2344160b1d68351347dd
ts <15313871502@163.com>
2019-05-10 15:07:48 +0800

    提交

---
+++ DataSource/ssm.sql
@@ -0,0 +1,37 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50714
+Source Host           : localhost:3306
+Source Database       : ssm
+
+Target Server Type    : MYSQL
+Target Server Version : 50714
+File Encoding         : 65001
+
+Date: 2019-05-10 15:06:52
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) DEFAULT NULL,
+  `sex` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('16', 'tx2', '2', null, '1');
+INSERT INTO `user` VALUES ('23', 'tx2', '2', null, '1');
+INSERT INTO `user` VALUES ('25', 'tx2', '2', null, '1');
+INSERT INTO `user` VALUES ('26', 'tx2', '2', null, '1');
